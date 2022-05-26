c9e4c3796c2252972d230b157ec430d86b5888d9
Youri Karijopawiro <yourikarijo1405@gmail.com>
2022-05-25 21:43:51 -0300

    updated controllers

--- app/Http/Controllers/AfspraakController.php
+++ app/Http/Controllers/AfspraakController.php
@@ -9,12 +9,15 @@ class AfspraakController extends Controller
     function add (){
         return view('afspraakadd');
     }
+
     function archive (){
         return view('afspraakarchive');
     }
+
     function edit (){
         return view('afspraakedit');
     }
+    
     function view (){
         return view('afspraakview');
     }
