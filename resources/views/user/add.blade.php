da9111aff11f95e68c9e901bb217b1555214cd4f
your1405 <yourikarijo1405@gmail.com>
2022-06-01 21:32:16 -0300

    added support for adding new users

--- resources/views/user/add.blade.php
+++ resources/views/user/add.blade.php
@@ -4,30 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User toevoegen</title>
+    <link rel="stylesheet" href="{{ asset('/storage/styles/main-styles.css') }}">
+    <title>Mental Massage | Gebruiker Toevoegen</title>
 </head>
 <body>
-    <h1>User toevoegen :)</h1>
-    
+    <h1>Gebruiker toevoegen</h1>
+    <a href="/dashboard">Terug naar dashboard</a>
     <!-- This is Login Gegevens --> 
-<div> 
+    <div> 
+        <form class="flex-column user-registration-form gap-medium" action="/user/add" method="POST">
+            @csrf
+            <label for="userEmail">Email Adress</label>
+            <input type="email" id="userEmail" name="userEmail" placeholder="Email Adress...">
 
-<form>
+            <label for="userPassword">Wachtwoord:</label>
+            <input type="password" id="userPassword" name="userPassword" placeholder="Wachtwoord">
+            {{-- <input type="button" value="Generate Password"> --}}
 
-<label for="Eadress">Email Adress</label><br>
-<input type="text" id="Eadress" name="Eadress"><br>
+            <label for="userAccountType"> Account Type: </label>
+            <select name="userAccountType" id="userAccountType">
+                <option value="1">Specialist</option>
+                <option value="2">Admin</option>
+            </select>
 
-<label for="Pass">Password:</label><br>
-<input type="password" id="Pass" name="Pass">
-<input type="button" value="Generate Password">
-
-<label for="AccType"> Account Type: </label><br>
-<input type="text" id="AccType" name="AccType">
-
-<input type="submit" value="Submit">
-
-</form>
-
+            <input type="submit" value="Voeg nieuwe medewerker toe">
+        </form>
+        @if($insertStatus == 'success')
+            <p>User met userid: {{ $userId }} successvol toegevoegd</p>
+        @elseif($insertStatus == 'userExists')
+            <p>User met email: {{ $email }} bestaat al!</p>
+        @endif
     </div>
 </body>
 </html>
