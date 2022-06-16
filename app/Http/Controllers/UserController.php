49a5ef7d8469ab254702324703053fee70e00c2d
your1405 <yourikarijo1405@gmail.com>
2022-06-15 21:02:51 -0300

    added gebruikers overzicht backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Medewerker;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Translation\Provider\NullProvider;
-use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Hash;
+use PhpParser\Node\Expr\NullsafeMethodCall;
 
 class UserController extends Controller
 {
@@ -25,13 +26,20 @@ class UserController extends Controller
             if(sizeOf($userAccountArray) == 0){
                 return view('login', ['loginError'=>'userNotExists']);
             } else {
+                $userId = $userAccountArray[0]['userId'];
                 if($email == $userAccountArray[0]['userEmail']){
                     if (Hash::check($password, $userAccountArray[0]['userPassword'])){                
                         if($userAccountArray[0]['userAccountTypeId'] == 2){
-                            session(['userType'=>'admin']);
+                            session([
+                                'userType'=>'admin',
+                                'userId'=>$userId
+                            ]);
                             return redirect('/dashboard');
                         } else if ($userAccountArray[0]['userAccountTypeId'] == 1){
-                            session(['userType'=>'specialist']);
+                            session([
+                                'userType'=>'specialist',
+                                'userId'=>$userId
+                            ]);
                             return redirect('/dashboard');
                         } else {
                             return view('login', ['loginError'=>'email']);
@@ -55,7 +63,7 @@ class UserController extends Controller
                 'email'=>null,
                 'userType'=>'admin'
             ]);
-        } else if ($request->isMethod('post')){
+        } else if ($request->isMethod('POST')){
             $userInfo = $request->all();
 
             $email = $userInfo['userEmail'];
@@ -67,12 +75,19 @@ class UserController extends Controller
                 DB::table('medewerker')->insert([
                     'userAccountTypeId' => $acctype,
                     'userEmail' => $email,
-                    'userPassword' => Hash::make($password),
+                    'userPassword' => Hash::make($password)
                 ]);
 
-                $userId = DB::table('medewerker')->select('userId')->where('userEmail', '=', $email)->get();
+                $userIdArray = DB::table('medewerker')->select('userId')->where('userEmail', '=', $email)->get()->toArray();
+
+                $userId = json_decode(json_encode($userIdArray), true);
+
+                DB::table('medewerker_info')->insert([
+                    'userId'=>$userId[0]['userId']
+                ]);
+
                 return view('user.add', [
-                    'userId'=>$userId,
+                    'userId'=>$userId[0]['userId'],
                     'insertStatus'=>'success',
                     'email'=>$email,
                     'userType'=>'admin'
@@ -100,7 +115,18 @@ class UserController extends Controller
         if($request->isMethod('get')){
             $userInfo = DB::table('medewerker')
             ->join('useraccounttype', 'medewerker.userAccountTypeId', '=', 'useraccounttype.userAccountTypeId')
-            ->select(['medewerker.userId', 'useraccounttype.userAccountDescription', 'medewerker.userEmail'])
+            ->join('medewerker_info', 'medewerker.userId', '=', 'medewerker_info.userId')
+            ->select([
+                'medewerker.userId',
+                'medewerker.userEmail',
+                'medewerker_info.userVoornaam',
+                'medewerker_info.userNaam', 
+                'useraccounttype.userAccountDescription',
+                'medewerker_info.userGeboorteDatum',
+                'medewerker_info.userGeslacht', 
+                'medewerker_info.userProfielfoto', 
+                'medewerker_info.userSpecialty'
+            ])
             ->get();
 
             $userInfoArray = json_decode(json_encode($userInfo->toArray()), true);
@@ -111,8 +137,11 @@ class UserController extends Controller
         }
     }
 
-    function view(){
-        return view('user.view');
+    function view(Request $request, $id){
+        // if($request->isMethod('GET')){
+        //     DB::table()
+        //     return view('user.view');
+        // }
     }
 
     function logout(Request $request){
