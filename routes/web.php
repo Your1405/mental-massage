cafa0507576f69f4f16f21c1c68ac57731efba36
your1405 <yourikarijo1405@gmail.com>
2022-06-15 21:24:13 -0300

    added comments

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,24 @@ use App\Http\Controllers\ClientController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| Handles different GET and POST requests 
+
+    - A GET request is usually used when a user needs to get/retrieve files or data from a webserver
+
+    - A POST request is usually used when a user needs to post/send files or data to a webserver
+  
 */
 
+/*
+    When a user submits a http GET request to '/dashboard',
+    the dashboard() function inside of the 'DashboardController' is called and returns either a view or a redirect
+*/
 Route::get("/dashboard", [DashboardController::class, 'dashboard']);
 Route::get('/', function (){
     return redirect('/login');
 });
 
+// This defines all the routes for the ClientController class
 Route::controller(ClientController::class)->group(function(){
     Route::get("/clienten", 'view');
     Route::get("/clienten/edit",'edit');
@@ -30,6 +40,7 @@ Route::controller(ClientController::class)->group(function(){
     Route::post("/clienten/add",'add');
 });
 
+// This defines all the routes for the AfspraakController class
 Route::controller(AfspraakController::class)->group(function(){
     Route::get("/afspraak", 'view');
     Route::get("/afspraak/edit",'edit');
@@ -37,6 +48,7 @@ Route::controller(AfspraakController::class)->group(function(){
     Route::get("/afspraak/add",'add');
 });
 
+// This defines all the routes for the UserController class
 Route::controller(UserController::class)->group(function(){
     Route::get('/login', 'login');
     Route::post('/login', 'login');
